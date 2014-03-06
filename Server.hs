515ae00a961c3bd27b82c3a52a8e6e537295f817
Benjamin Summers <bsummer4@eecs.utk.edu>
2014-03-06 23:23:36 +0000

    Added support for username.

--- Server.hs
+++ Server.hs
@@ -1,33 +1,52 @@
-{-# LANGUAGE OverloadedStrings, UnicodeSyntax #-}
+{-# LANGUAGE OverloadedStrings, UnicodeSyntax, ScopedTypeVariables #-}
 
-import Data.Char (isPunctuation, isSpace)
-import Data.Monoid (mappend)
-import Data.Text (Text)
 import Control.Exception (finally)
+import Data.Monoid (mappend)
 import Control.Monad (forM_, forever)
-import Control.Concurrent (MVar, newMVar, modifyMVar_, modifyMVar, readMVar)
-import Control.Monad.IO.Class (liftIO)
+import Control.Concurrent
+import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import Data.Text.Encoding as E
 import qualified Network.WebSockets as WS
 
-broadcast ∷ Text → [WS.Connection] → IO ()
-broadcast message clients = do
+
+-- Message encoding.
+encodeMsg ∷ (Text,Text) → Text
+encodeMsg (user,msg) = user `mappend` ":" `mappend` msg
+
+decodeMsg ∷ Text → Maybe (Text,Text)
+decodeMsg str = case T.breakOn "::" str of
+	(user,"") → Nothing
+	(user,tail) → Just (user, T.drop 2 tail)
+
+
+-- Server State
+type User = Text
+type Client = (User, WS.Connection)
+
+removeClient ∷ User → [Client] → [Client]
+removeClient user = filter ((/= user) . fst)
+
+
+-- Server
+broadcast ∷ Text → Text → [WS.Connection] → IO()
+broadcast user message clients = do
 	T.putStrLn message
-	forM_ clients $ \conn → WS.sendTextData conn message
+	let e = encodeMsg (user,message)
+	forM_ clients $ \conn → WS.sendTextData conn e
 
-application ∷ MVar [WS.Connection] → WS.PendingConnection → IO ()
-application state pending = do
+application ∷ MVar[Client] → WS.PendingConnection → IO()
+application stateV pending = do
 	conn ← WS.acceptRequest pending
-	liftIO $ modifyMVar_ state $ return . (conn:)
-	talk conn state
+	user∷Text ← WS.receiveData conn
+	modifyMVar_ stateV $ return . ((user,conn):)
+	let disconnect = modifyMVar_ stateV $ return . removeClient user
+	flip finally disconnect $ forever $ do
+		msg∷Text ← WS.receiveData conn
+		readMVar stateV >>= broadcast user msg . map snd
 
-talk ∷ WS.Connection → MVar[WS.Connection] → IO ()
-talk conn state = forever $ do
-	msg ← WS.receiveData conn
-	liftIO $ readMVar state >>= broadcast msg
-
-main ∷ IO ()
+main ∷ IO()
 main = do
-	state ← newMVar([]∷[WS.Connection])
+	state ← newMVar([]∷[Client])
 	WS.runServer "0.0.0.0" 9160 $ application state
