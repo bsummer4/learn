9e255cf47b67afeff6d81a5db76146f9941f18bc
Benjamin Summers <ben@benjaminsummers.net>
2015-01-19 04:08:07 +0000

    Reorganization.

---
+++ record/shell.nix
@@ -0,0 +1,12 @@
+let pkgs = (import <nixpkgs> {});
+    haskellPackages = pkgs.recurseIntoAttrs (pkgs.haskellPackages.override {
+    extension = self : super :
+    let callPackage = self.callPackage;
+    in {
+       prettyCompact = callPackage ./nix/pretty-compact.nix {};
+       thisPackage = haskellPackages.callPackage (import ./default.nix) {};
+    };});
+in pkgs.lib.overrideDerivation haskellPackages.thisPackage (old: {
+   buildInputs = old.buildInputs ++ [
+     haskellPackages.cabalInstall
+   ];})
