f06ff22423eca7fb57a3d67a544d90f7b5c6c54d
Benjamin Summers <ben@benjaminsummers.net>
2015-02-07 00:57:34 +0100

    Password-free ssh logins, and mosh.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,16 +1,15 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [ ./hardware-configuration.nix ];
 
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/vda";
 
   networking.hostId = "2bc28922";
+  networking.firewall.allowedUDPPorts = [60001]; # For Mosh
+  networking.firewall.allowPing = true;
 
   i18n = {
     consoleFont="lat9w-16";
@@ -18,7 +17,13 @@
     defaultLocale="en_US.UTF-8";
   };
 
+  environment.systemPackages = with pkgs; [
+    wget curl mosh
+  ];
+
   services.openssh.enable = true;
+  services.openssh.permitRootLogin = "no";
+
   security.sudo.enable = true;
 
   users.extraUsers.ben = {
@@ -26,5 +31,7 @@
     uid = 1000;
     group = "users";
     extraGroups = ["wheel"];
+    openssh.authorizedKeys.keys =
+      [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDs7c+tW9nfevShFfHyNWzsl/wXYKZh8UoXBOOyCoDLa8blwrLFmTOEOJY2hdX58YlLSJbYvponUMQOJoCUrlS1Sj3CH+MGR9e/XzS0+js4QtzTvof22qUB4wqyeCHD9XRDR1YHgOfiIeS6XyQFqMuTNh5/rKTPff5bjWpfcMRa+aGcLRcE9FYDYIjeqVOMWYL+oPTEr5c4I9MXoiJMZfOfz57pHHpsnBrQawSW1WV5xqOeFZFZiNyGHPyWTKrTjIgVC9bY0CYqiOM9wc6VJtuzz+BiyDlpPaHlBJNd6JvSsqkIBeb4etXyWeEpfNbd20LE68NDoqd0/AJWCioPG39P b@benjamins-air" ];
   };
 }
